38d39c11b564aca79b89b3ec0c8398f2ef08755e
Celeste <celeste-quinn@live.nmit.ac.nz>
2020-03-10 12:05:12 +1300

    completed lab two exercise

---
+++ index.js
@@ -0,0 +1,13 @@
+let x = 2
+let y = 5
+let z = 3
+
+let sumOne = Math.pow(x, 3); //uses math.pow function to work out variable x to the power of 3 (x cubed)
+let sumTwo = Math.pow(y, 3);
+let sumThree = Math.pow(z, 3);
+
+console.log(sumOne)
+console.log(sumTwo)
+console.log(sumThree)
+
+console.log(sumOne + sumTwo + sumThree) //the sum of all cubed numbers added up
